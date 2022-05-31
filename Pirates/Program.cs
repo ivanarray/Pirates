25a8b06ad7d72ad22372c11315aa7fca19db829a
ivanarray <i.maliamow@yandex.ru>
2022-05-31 16:23:52 +0500

    formatting

--- Pirates/Program.cs
+++ Pirates/Program.cs
@@ -30,6 +30,99 @@ namespace Pirates
         }
     }
 
+    public class LaserGun
+    {
+        public decimal CurrentTimeInMinutes;
+        private readonly decimal maxSpeed;
+        private CyclicLinkList<Ship> ships;
+
+        public LaserGun(decimal azimuth, decimal maxSpeed, List<Ship> pirates)
+        {
+            this.maxSpeed = maxSpeed;
+            var startPosition = new Ship(azimuth, 0, 1, 0);
+            pirates.Add(startPosition);
+            pirates.Sort((x, y) => x.Azimuth.CompareTo(y.Azimuth));
+            ships = new CyclicLinkList<Ship>(pirates);
+        }
+
+
+        public List<Ship> Process()
+        {
+            var res = new List<Ship>();
+            ships = ships.First(x => x.Value.Id == 0);
+
+            while (!ships.IsSingleValue)
+            {
+                var min = ships
+                    .Where(x => x.Value.Id != ships.Value.Id)
+                    .Min(x => x.Value);
+
+                var l = ships.Value.GetTimeToOther(ships.Left!.Value, maxSpeed);
+                var r = ships.Value.GetTimeToOther(ships.Right!.Value, maxSpeed);
+
+                var near = l < r ? ships.Left : ships.Right;
+
+                var timeToNear = ships.Value.GetTimeToOther(near.Value, maxSpeed);
+                if (timeToNear == 0)
+                {
+                    res.Add(near.Value);
+                    near = near.Left;
+                    near!.RemoveRight();
+                    continue;
+                }
+
+                var timeFromNearToMin = near.Value.GetTimeToOther(min, maxSpeed);
+                var timeToNearAndMin = timeToNear + timeFromNearToMin;
+                var next = timeToNearAndMin + CurrentTimeInMinutes < min.TimeToTargetInMinutes ? near.Value : min;
+
+                var offsetAngle = next.Azimuth - ships.Value.Azimuth;
+                offsetAngle = offsetAngle >= 0 ? offsetAngle : offsetAngle + 360;
+
+
+                if (offsetAngle <= 180)
+                {
+                    res.Add(ships.Right!.Value);
+                    CurrentTimeInMinutes += offsetAngle / (maxSpeed * 360);
+                    if (CurrentTimeInMinutes > next.TimeToTargetInMinutes)
+                    {
+                        return null;
+                    }
+
+                    ships = ships.Right;
+                    ships.RemoveLeft();
+                }
+                else
+                {
+                    res.Add(ships.Left!.Value);
+                    CurrentTimeInMinutes += (360 - offsetAngle) / (maxSpeed * 360);
+                    if (CurrentTimeInMinutes > next.TimeToTargetInMinutes)
+                    {
+                        return null;
+                    }
+
+                    ships = ships.Left;
+                    ships.RemoveRight();
+                }
+            }
+
+            return res;
+        }
+
+        public string GetResultInString()
+        {
+            var res = Process();
+            if (res is null) return "Impossible";
+            var sb = new StringBuilder();
+            sb.Append($"{CurrentTimeInMinutes.ToString("F3", CultureInfo.InvariantCulture)}\n");
+            foreach (var ship in res)
+            {
+                sb.Append($"{ship.Id}\n");
+            }
+
+            return sb.ToString();
+        }
+    }
+
     public readonly struct Ship : IComparable<Ship>, IEquatable<Ship>, IComparable
     {
         public Ship(decimal azimuth, decimal distance, decimal speed, int id)
@@ -202,98 +295,6 @@ namespace Pirates
             return $"Value: {Value}\n" +
                    $"Left: {Left.Value}\n" +
                    $"Right: {Right.Value}";
-        }
-    }
-
-    public class LaserGun
-    {
-        public decimal CurrentTimeInMinutes;
-        private readonly decimal maxSpeed;
-        private CyclicLinkList<Ship> ships;
-
-        public LaserGun(decimal azimuth, decimal maxSpeed, List<Ship> pirates)
-        {
-            this.maxSpeed = maxSpeed;
-            var startPosition = new Ship(azimuth, 0, 1, 0);
-            pirates.Add(startPosition);
-            pirates.Sort((x, y) => x.Azimuth.CompareTo(y.Azimuth));
-            ships = new CyclicLinkList<Ship>(pirates);
-        }
-
-
-        public List<Ship>? Process()
-        {
-            var res = new List<Ship>();
-            ships = ships.First(x => x.Value.Id == 0);
-
-            while (!ships.IsSingleValue)
-            {
-                var min = ships
-                    .Where(x => x.Value.Id != ships.Value.Id)
-                    .Min(x => x.Value);
-
-                var l = ships.Value.GetTimeToOther(ships.Left!.Value, maxSpeed);
-                var r = ships.Value.GetTimeToOther(ships.Right!.Value, maxSpeed);
-
-                var near = l < r ? ships.Left : ships.Right;
-
-                var timeToNear = ships.Value.GetTimeToOther(near.Value, maxSpeed);
-                if (timeToNear == 0)
-                {
-                    res.Add(near.Value);
-                    near = near.Left;
-                    near!.RemoveRight();
-                }
-
-                var timeFromNearToMin = near.Value.GetTimeToOther(min, maxSpeed);
-                var timeToNearAndMin = timeToNear + timeFromNearToMin;
-                var next = timeToNearAndMin + CurrentTimeInMinutes < min.TimeToTargetInMinutes ? near.Value : min;
-
-                var offsetAngle = next.Azimuth - ships.Value.Azimuth;
-                offsetAngle = offsetAngle >= 0 ? offsetAngle : offsetAngle + 360;
-
-
-                if (offsetAngle <= 180)
-                {
-                    res.Add(ships.Right!.Value);
-                    CurrentTimeInMinutes += offsetAngle / (maxSpeed * 360);
-                    if (CurrentTimeInMinutes > next.TimeToTargetInMinutes)
-                    {
-                        return null;
-                    }
-
-                    ships = ships.Right;
-                    ships.RemoveLeft();
-                }
-                else
-                {
-                    res.Add(ships.Left!.Value);
-                    CurrentTimeInMinutes += (360 - offsetAngle) / (maxSpeed * 360);
-                    if (CurrentTimeInMinutes > next.TimeToTargetInMinutes)
-                    {
-                        return null;
-                    }
-
-                    ships = ships.Left;
-                    ships.RemoveRight();
-                }
-            }
-
-            return res;
-        }
-
-        public string GetResultInString()
-        {
-            var res = Process();
-            if (res is null) return "Impossible";
-            var sb = new StringBuilder();
-            sb.Append($"{CurrentTimeInMinutes.ToString("F3", CultureInfo.InvariantCulture)}\n");
-            foreach (var ship in res)
-            {
-                sb.Append($"{ship.Id}\n");
-            }
-
-            return sb.ToString();
         }
     }
 }
